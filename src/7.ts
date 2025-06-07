879eb50ea910173b9b9e9ca2fb238949a55fc3b4
yossuli <s1f102302348@iniad.org>
2025-06-08 01:41:41 +0900

    0608

--- src/7.ts
+++ src/7.ts
@@ -1,39 +1,48 @@
 import type { ArrayFrom } from "./3/arrayFrom";
 import type { hoge_1, hoge_2_1 } from "./5";
-type NNNum = [number, number, number];
+
+type MatchPre<
+  A extends number[],
+  Out extends number[],
+  Rest extends number[],
+  Acc extends number[] = [],
+  Fg extends number[] = [],
+> = Out extends [...Acc, ...infer T extends [number, number], ...number[]]
+  ? T extends [0, -1]
+    ? hoge<A, hoge_1<A, Out, Acc>, Rest, [...Acc, number], [...Fg, 1]>
+    : hoge<A, Out, Rest, [...Acc, number], Fg>
+  : Out;
+
+type MatchSuf<
+  A extends number[],
+  Out extends number[],
+  Rest extends number[],
+  Acc extends number[] = [],
+  Fg extends number[] = [],
+> = Acc extends [...infer First extends number[], number]
+  ? Out extends [...First, ...infer T extends [number, number], ...number[]]
+    ? T extends [-1, 0]
+      ? hoge<A, hoge_2_1<A, Out, Acc>, Rest, [...Acc, number], [...Fg, 1]>
+      : Out
+    : never
+  : never;
+
 type hoge<
   A extends number[],
-  Out extends number[] = ArrayFrom<-1, A["length"]>,
+  Out extends number[],
   Mask extends number[] = ArrayFrom<number, A["length"]>,
   Acc extends number[] = [],
   Fg extends number[] = [],
-> = Mask extends [number, ...infer Rest extends number[]]
+> = Mask extends [number, ...infer Rest extends [...number[]]]
   ? Acc["length"] extends 0
-    ? Out extends [...infer T extends [number, number], ...number[]]
-      ? T extends [0, -1]
-        ? hoge<A, hoge_1<A, Out, []>, Rest, [...Acc, number], [...Fg, 1]>
-        : hoge<A, Out, Rest, [...Acc, number], [...Fg]>
-      : never
-    : Mask["length"] extends 1
-      ? Acc extends [...infer F extends number[], number]
-        ? Out extends [...F, ...infer T extends [number, number]]
-          ? T extends [-1, 0]
-            ? hoge<A, hoge_2_1<A, Out, Acc>, Rest, [...Acc, number], [...Fg, 1]>
-            : hoge<A, Out, Rest, [...Acc, number], [...Fg]>
-          : never
-        : never
-      : Acc extends [...infer F extends number[], number]
-        ? Out extends [...F, ...infer T extends NNNum, ...number[]]
-          ? T extends [-1, 0, number] | [number, 0, -1]
-            ? hoge<A, hoge_2_1<A, Out, Acc>, Rest, [...Acc, number], [...Fg, 1]>
-            : hoge<A, Out, Rest, [...Acc, number], [...Fg]>
-          : never
-        : never
+    ? MatchPre<A, Out, Rest, Acc, Fg>
+    : MatchPre<A, MatchSuf<A, Out, Rest, Acc, Fg>, Rest, Acc, Fg>
   : Fg["length"] extends 0
     ? Out
     : hoge<A, Out, ArrayFrom<number, A["length"]>, []>;
 type S = -1;
-type hoge_r = hoge<[0, 0, 1, 1, 1, 0, 0], [0, -1, -1, -1, -1, -1, 0]>;
+
+type hoge_r = hoge<[0, 0, 1, 0, 0], [-1, 0, -1, -1, 0]>;
 type hoge_r2 = hoge<
   [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0],
   [0, S, S, S, S, S, 0, S, S, S, S, S, S, 0]
