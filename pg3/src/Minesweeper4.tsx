94fdc94070faf87cdabb730d4c09c0f62b2ddee1
yossuli <s1f102302348@iniad.org>
2025-07-31 19:38:23 +0900

    chore: 2 copy Minsweeper.tsx

---
+++ pg3/src/Minesweeper4.tsx
@@ -0,0 +1,67 @@
+import "./App.css";
+import { useState } from "react";
+import {
+  Blank,
+  Board,
+  Conditional,
+  ExprFalse,
+  ExprTrue,
+  Stone,
+} from "./components";
+import { B, C, CLICK, STONE } from "./constants";
+import { useCache } from "./hooks";
+import type { Board as BoardType, BombMap, CLICK_TYPE, Pos } from "./types";
+import { genBoard, open } from "./utils";
+
+export const Minesweeper4 = () => {
+  console.time("Minesweeper4");
+  const [bombMap, setBombMap] = useState<BombMap>(
+    // genBoard({ width: 9, height: 9 }, 0)
+    genBoard({ width: 9, height: 9 }, 0).map((row, i) =>
+      row.map((_, j) => (i === j || (i === 0 && j === 8) ? B : C)),
+    ),
+  );
+  const [ClickHistory, setClickHistory] = useState<
+    { x: number; y: number; type: CLICK_TYPE }[]
+  >([]);
+  const board: BoardType = genBoard({ width: 9, height: 9 }, STONE);
+
+  const clickHandler = ({ x, y }: Pos) => {
+    if (ClickHistory.length === 0) {
+      // setBombMap(genBombs({ width: W, height: H, bombs: BN }, { x, y }));
+    }
+    setClickHistory((prev) => [...prev, { x, y, type: CLICK }]);
+  };
+
+  const cachedOpen = useCache(open);
+
+  const computedBoard = ClickHistory.reduce((acc, { x, y, type }) => {
+    if (type === CLICK) {
+      return cachedOpen({ x, y }, acc, bombMap, cachedOpen);
+    }
+    return acc;
+  }, board);
+  console.timeEnd("Minesweeper4");
+  return (
+    <div className="App">
+      <Board board={computedBoard}>
+        {(cell, x, y) => (
+          <Conditional condition={cell === STONE}>
+            <ExprTrue>
+              <Stone open={() => clickHandler({ x, y })} />
+            </ExprTrue>
+            <ExprFalse>
+              <Blank number={cell} />
+            </ExprFalse>
+          </Conditional>
+        )}
+      </Board>
+      <button
+        type="button"
+        onClick={() => setClickHistory((prev) => prev.slice(0, -1))}
+      >
+        Back
+      </button>
+    </div>
+  );
+};
